60b9099b17c092c8480f825b8d21bfa247df9f80
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2021-03-19 12:04:18 -0700

    made SEM a function and updated all the sites

--- Scripts/SantosSGDScript.R
+++ Scripts/SantosSGDScript.R
@@ -15,6 +15,7 @@ library(suncalc)
 library(brms)
 library(tidybayes)
 library(bayesplot)
+library(stringr)
 
 ##### Read in the data #############
 # SGDData<-read_csv(here("Data","Timeseries_edited.csv")) %>%
@@ -144,47 +145,52 @@ CO2mod<-bf(excess_co2_umol_kg_std ~DayNight*(rn_bq_m3_std+aou_umol_l_std))
 ## Later
 # Delta TA ~ pH + temp
 
-# run the SEM for Heron Island
-heron_fit_brms <- brm(SGDmod+
+
+
+# Function to run Bayesian SEM and make the posterior predictive checks and plot marginal effects
+RunSEM<-function(site){
+  
+
+fit_brms <- brm(SGDmod+
                     Nmod+
                     Tempmod+ 
                     pHmod+
                     AOUmod +
                     CO2mod+
                     set_rescor(FALSE),
-                  data=SGDData[SGDData$site == "Heron Island",]
+                  data=SGDData[SGDData$site == site,]
                   ,cores=4, chains = 3)
 # calculate LOO (leave one out) diagnostics
-SGD_loo_heron<-loo(heron_fit_brms, reloo = TRUE) # looks good!
+SGD_loo<-loo(fit_brms, reloo = TRUE) # looks good!
 
-p1<-pp_check(heron_fit_brms, resp="rnbqm3std") +
+p1<-pp_check(fit_brms, resp="rnbqm3std") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("Radon")
-p2<-pp_check(heron_fit_brms, resp="noxumstd") +
+p2<-pp_check(fit_brms, resp="noxumstd") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("N+N")
-p3<-pp_check(heron_fit_brms, resp="temperaturestd") +
+p3<-pp_check(fit_brms, resp="temperaturestd") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("Temperature")
-p4<-pp_check(heron_fit_brms, resp="phinstd") +
+p4<-pp_check(fit_brms, resp="phinstd") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("pH")
-p5<-pp_check(heron_fit_brms, resp="aouumollstd") +
+p5<-pp_check(fit_brms, resp="aouumollstd") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("AOU")
-p6<-pp_check(heron_fit_brms, resp="excessco2umolkgstd") +
+p6<-pp_check(fit_brms, resp="excessco2umolkgstd") +
   scale_color_manual(values=c("red", "black"))+
   ggtitle("Excess CO2")
 
 p1+p2+p3+p4+p5+p6+plot_layout(guides = "collect") +
   plot_annotation(title = 'Heron Island Posterior Predictive Checks', tag_levels = "A")+
-  ggsave("Output/Posteriorchecks_HeronIsland.pdf", width = 5, height = 5)
+  ggsave(here("Output",paste(site,"Posteriorchecks.pdf")), width = 5, height = 5)
 
 # plot the results
 # Model 1
 # SGD ~ depth
 
-R<-conditional_effects(heron_fit_brms, "water_level_m_std", resp = "rnbqm3std", method = "predict", resolution = 1000)
+R<-conditional_effects(fit_brms, "water_level_m_std", resp = "rnbqm3std", method = "predict", resolution = 1000)
 R1<-R$rnbqm3std.rnbqm3std_water_level_m_std %>% # back transform the scaled effects for the plot
   mutate(estimate = estimate__*attr(SGDData$rn_bq_m3_std,"scaled:scale")+attr(SGDData$rn_bq_m3_std,"scaled:center"),
          lower = lower__*attr(SGDData$rn_bq_m3_std,"scaled:scale")+attr(SGDData$rn_bq_m3_std,"scaled:center"),
@@ -194,7 +200,7 @@ R1<-R$rnbqm3std.rnbqm3std_water_level_m_std %>% # back transform the scaled effe
   ggplot()+ # back trasform the log transformed data for better visual
   geom_line(aes(x = WaterLevel, y = estimate), lwd = 1, color = 'grey')+
   geom_ribbon(aes(x = WaterLevel,ymin=lower, ymax=upper), linetype=1.5, alpha=0.3, fill = "grey")+
-  geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = water_level_m, y = rn_bq_m3, color = DayNight)) +
+  geom_point(data = SGDData[SGDData$site==site,], aes(x = water_level_m, y = rn_bq_m3, color = DayNight)) +
   xlab("Water level (m)")+
   ylab(expression(atop("Radon", paste("(bq m"^3,")"))))+
   ggtitle("Model 1")+
@@ -205,7 +211,7 @@ R1<-R$rnbqm3std.rnbqm3std_water_level_m_std %>% # back transform the scaled effe
 
 ## Model 2
 # N ~ SGD*DayNight
-R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "noxumstd", method = "predict", resolution = 1000)
+R<-conditional_effects(fit_brms, "rn_bq_m3_std:DayNight", resp = "noxumstd", method = "predict", resolution = 1000)
 R2<-R$`noxumstd.noxumstd_rn_bq_m3_std:DayNight` %>% # back transform the scaled effects for the plot
   mutate(estimate = estimate__*attr(SGDData$nox_u_m_std,"scaled:scale")+attr(SGDData$nox_u_m_std,"scaled:center"),
          lower = lower__*attr(SGDData$nox_u_m_std,"scaled:scale")+attr(SGDData$nox_u_m_std,"scaled:center"),
@@ -215,7 +221,7 @@ R2<-R$`noxumstd.noxumstd_rn_bq_m3_std:DayNight` %>% # back transform the scaled 
   ggplot()+ # back trasform the log transformed data for better visual
   geom_line(aes(x = radon, y = estimate, color = DayNight), lwd = 1)+
   geom_ribbon(aes(x = radon,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-  geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = rn_bq_m3, y = nox_u_m, color = DayNight)) +
+  geom_point(data = SGDData[SGDData$site==site,], aes(x = rn_bq_m3, y = nox_u_m, color = DayNight)) +
   ylab(expression(atop("Nitrate + Nitrite", paste("(",mu, "mol L"^-1,")"))))+
   xlab(expression(atop("Radon", paste("(bq m"^3,")"))))+
   ggtitle("Model 2")+
@@ -227,7 +233,7 @@ R2<-R$`noxumstd.noxumstd_rn_bq_m3_std:DayNight` %>% # back transform the scaled 
   # Model 3
   # Temperature ~ SGD*DayNight
   
-R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperaturestd", method = "predict", resolution = 1000)
+R<-conditional_effects(fit_brms, "rn_bq_m3_std:DayNight", resp = "temperaturestd", method = "predict", resolution = 1000)
   R3<-R$`temperaturestd.temperaturestd_rn_bq_m3_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$temperature_std,"scaled:scale")+attr(SGDData$temperature_std,"scaled:center"),
            lower = lower__*attr(SGDData$temperature_std,"scaled:scale")+attr(SGDData$temperature_std,"scaled:center"),
@@ -237,7 +243,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = radon, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = radon,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = rn_bq_m3, y = temperature, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = rn_bq_m3, y = temperature, color = DayNight)) +
     ylab(expression(atop("Temperature",paste("(", degree, "C)"))))+
   xlab(expression(atop("Radon", paste("(bq m"^3,")"))))+
     ggtitle("Model 3")+
@@ -248,7 +254,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
   
   # Model 4a
   # pH ~ DayNight*(AOU + SGD)
-  R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "phinstd", method = "predict", resolution = 1000)
+  R<-conditional_effects(fit_brms, "rn_bq_m3_std:DayNight", resp = "phinstd", method = "predict", resolution = 1000)
   R4<-R$`phinstd.phinstd_rn_bq_m3_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$p_h_in_std,"scaled:scale")+attr(SGDData$p_h_in_std,"scaled:center"),
            lower = lower__*attr(SGDData$p_h_in_std,"scaled:scale")+attr(SGDData$p_h_in_std,"scaled:center"),
@@ -258,7 +264,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = radon, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = radon,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = rn_bq_m3, y = p_h_in, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = rn_bq_m3, y = p_h_in, color = DayNight)) +
     ylab("pH")+
     xlab(expression(atop("Radon", paste("(bq m"^3,")"))))+
     ggtitle("Model 4")+
@@ -269,7 +275,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
   
   # Model 4b
   # pH ~ DayNight*(AOU + SGD)
-  R<-conditional_effects(heron_fit_brms, "aou_umol_l_std:DayNight", resp = "phinstd", method = "predict", resolution = 1000)
+  R<-conditional_effects(fit_brms, "aou_umol_l_std:DayNight", resp = "phinstd", method = "predict", resolution = 1000)
   R4b<-R$`phinstd.phinstd_aou_umol_l_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$p_h_in_std,"scaled:scale")+attr(SGDData$p_h_in_std,"scaled:center"),
            lower = lower__*attr(SGDData$p_h_in_std,"scaled:scale")+attr(SGDData$p_h_in_std,"scaled:center"),
@@ -279,7 +285,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = aou, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = aou,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = aou_umol_l, y = p_h_in, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = aou_umol_l, y = p_h_in, color = DayNight)) +
     ylab("pH")+
     xlab(expression(atop("AOU", paste("(",mu, "mol L"^-1,")"))))+
     ggtitle("Model 4")+
@@ -291,17 +297,17 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
   # Model 5
   # AOU ~ DayNight*(N+Temperature)
   
-  R<-conditional_effects(heron_fit_brms, "temperature_std:DayNight", resp = "aouumollstd", method = "predict", resolution = 1000)
+  R<-conditional_effects(fit_brms, "temperature_std:DayNight", resp = "aouumollstd", method = "predict", resolution = 1000)
   R5<-R$`aouumollstd.aouumollstd_temperature_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$aou_umol_l_std,"scaled:center"),
            lower = lower__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$aou_umol_l_std,"scaled:center"),
-           upper = upper__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$p_h_in_std,"scaled:center")) %>%
+           upper = upper__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$aou_umol_l_std,"scaled:center")) %>%
     mutate(temperature = temperature_std*attr (SGDData$temperature_std,"scaled:scale")+attr(SGDData$temperature_std,"scaled:center")
     )%>%
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = temperature, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = temperature,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = temperature, y = aou_umol_l, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = temperature, y = aou_umol_l, color = DayNight)) +
     xlab(expression(atop("Temperature",paste("(", degree, "C)"))))+
     ylab(expression(atop("AOU", paste("(",mu, "mol L"^-1,")"))))+
     ggtitle("Model 5")+
@@ -313,7 +319,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
   # Model 5b
   # AOU ~ DayNight*(N+Temperature)
   
-  R<-conditional_effects(heron_fit_brms, "nox_u_m_std:DayNight", resp = "aouumollstd", method = "predict", resolution = 1000)
+  R<-conditional_effects(fit_brms, "nox_u_m_std:DayNight", resp = "aouumollstd", method = "predict", resolution = 1000)
   R5b<-R$`aouumollstd.aouumollstd_nox_u_m_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$aou_umol_l_std,"scaled:center"),
            lower = lower__*attr(SGDData$aou_umol_l_std,"scaled:scale")+attr(SGDData$aou_umol_l_std,"scaled:center"),
@@ -323,7 +329,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = NN, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = NN,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = nox_u_m, y = aou_umol_l, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = nox_u_m, y = aou_umol_l, color = DayNight)) +
     xlab(expression(atop("Nitrate + Nitrite", paste("(",mu, "mol L"^-1,")"))))+
     ylab(expression(atop("AOU", paste("(",mu, "mol L"^-1,")"))))+
     ggtitle("Model 5")+
@@ -335,7 +341,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "temperat
 # Model 6
 # excess CO2 ~ DayNight*(SGD +AOU)
   
-R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco2umolkgstd", method = "predict", resolution = 1000)
+R<-conditional_effects(fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco2umolkgstd", method = "predict", resolution = 1000)
   R6<-R$`excessco2umolkgstd.excessco2umolkgstd_rn_bq_m3_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$excess_co2_umol_kg_std,"scaled:scale")+attr(SGDData$excess_co2_umol_kg_std,"scaled:center"),
            lower = lower__*attr(SGDData$excess_co2_umol_kg_std,"scaled:scale")+attr(SGDData$excess_co2_umol_kg_std,"scaled:center"),
@@ -345,7 +351,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = radon, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = radon,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = rn_bq_m3, y = excess_co2_umol_kg, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = rn_bq_m3, y = excess_co2_umol_kg, color = DayNight)) +
     xlab(expression(atop("Radon", paste("(bq m"^3,")"))))+
     ylab(expression(atop("Excess CO"[2], paste("(",mu, "mol kg"^-1,")"))))+
     ggtitle("Model 6")+
@@ -357,7 +363,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco
   # Model 6b
   # excess CO2 ~ DayNight*(SGD +AOU)
   
-  R<-conditional_effects(heron_fit_brms, "aou_umol_l_std:DayNight", resp = "excessco2umolkgstd", method = "predict", resolution = 1000)
+  R<-conditional_effects(fit_brms, "aou_umol_l_std:DayNight", resp = "excessco2umolkgstd", method = "predict", resolution = 1000)
   R6b<-R$`excessco2umolkgstd.excessco2umolkgstd_aou_umol_l_std:DayNight` %>% # back transform the scaled effects for the plot
     mutate(estimate = estimate__*attr(SGDData$excess_co2_umol_kg_std,"scaled:scale")+attr(SGDData$excess_co2_umol_kg_std,"scaled:center"),
            lower = lower__*attr(SGDData$excess_co2_umol_kg_std,"scaled:scale")+attr(SGDData$excess_co2_umol_kg_std,"scaled:center"),
@@ -367,7 +373,7 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco
     ggplot()+ # back trasform the log transformed data for better visual
     geom_line(aes(x = aou, y = estimate, color = DayNight), lwd = 1)+
     geom_ribbon(aes(x = aou,ymin=lower, ymax=upper, fill = DayNight), linetype=1.5, alpha=0.3)+
-    geom_point(data = SGDData[SGDData$site=='Heron Island',], aes(x = aou_umol_l, y = excess_co2_umol_kg, color = DayNight)) +
+    geom_point(data = SGDData[SGDData$site==site,], aes(x = aou_umol_l, y = excess_co2_umol_kg, color = DayNight)) +
     xlab(expression(atop("AOU", paste("(",mu, "mol L"^-1,")"))))+
     ylab(expression(atop("Excess CO"[2], paste("(",mu, "mol kg"^-1,")"))))+
     ggtitle("Model 6")+
@@ -377,11 +383,24 @@ R<-conditional_effects(heron_fit_brms, "rn_bq_m3_std:DayNight", resp = "excessco
     theme(plot.title = element_text(hjust = 0.5, size = 14), legend.position = 'bottom')
   
   ## bring them all together in patchwork
-  R<-(R1|R2)/(R4|R4b)/(R5|R5b)/(R6|R6b)/(R3)+plot_layout(guides = "collect")+
+  R<-(R1|R2|R3)/(R4|R4b)/(R5|R5b)/(R6|R6b)+plot_layout(guides = "collect")+
     plot_annotation(tag_levels = "A")&
     theme(axis.title.x = element_text(size = 14),
           axis.title.y = element_text(size = 14),
           axis.text = element_text(size = 11))
   
-  ggsave("Output/marginaleffects_heronisland.pdf",R, width = 10, height = 18, useDingbats = FALSE)
+  ggsave(here("Output",paste(site,"marginaleffects.pdf")),R, width = 12, height = 18, useDingbats = FALSE)
   
+  var_name <- paste(fit_brms, site, sep="_") # Construct the name
+  var_name <- str_replace_all(var_name, " ", "")   # remove white space
+  return(fit_brms)
+  
+}
+
+# run the SEM for Heron Island
+HI_fit<-RunSEM(site ="Heron Island")
+CI_fit<-RunSEM(site ="Cook Islands")
+LI_fit<-RunSEM(site ="Lizard Island")
+OTI_fit<-RunSEM(site ="One Tree")
+LHI_fit<-RunSEM(site ="Lord Howe Island")
+NP_fit<-RunSEM(site ="Nusa Penida")
